7adb8dee2e84befa07a23f2086ce8287e596d8be
Nika Masimova <masimostudy#gmail.com>
2022-04-01 00:34:38 +0300

    fix: per reviewer's comments, 1st iteration

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,22 @@
-let addButton = document.querySelector(".profile__add-button");
 let editProfileButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".pop-up__close-button");
-let saveButton = document.querySelector(".pop-up__save-button");
 let popUp = document.querySelector(".pop-up");
 let currentName = document.querySelector(".profile__name");
 let currentBio = document.querySelector(".profile__bio");
 let formElement = document.querySelector(".pop-up__form")
-let nameInput = document.querySelectorAll(".pop-up__input")[0]
-let bioInput = document.querySelectorAll(".pop-up__input")[1]
+let nameInput = document.querySelector(".pop-up__input_name")
+let bioInput = document.querySelector(".pop-up__input_bio")
 
 function togglePopUp() {
     popUp.classList.toggle("pop-up_opened");
+    nameInput.value = currentName.textContent
+    bioInput.value = currentBio.textContent
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     currentName.textContent = nameInput.value
     currentBio.textContent = bioInput.value
-    console.log(currentName, currentBio)
     togglePopUp()
 }
 
