d864ca7e33cbddc0d4c51e7a3d7ee788729d55b7
Nika Masimova <masimostudy#gmail.com>
2022-04-09 23:35:51 +0300

    1.1

--- scripts/index.js
+++ scripts/index.js
@@ -22,6 +22,8 @@ const gallery = document.querySelector(".gallery");
 const imageTemplate = document.querySelector("#image-template").content;
 
 const popUpImageView = document.querySelector(".pop-up_image-view");
+const image = document.querySelector(".image-view__image");
+const imageName = document.querySelector(".image-view__name");
 const closeImageViewButton = document.querySelector(
   ".pop-up__close-button_image-view"
 );
@@ -30,26 +32,32 @@ const initialCards = [
   {
     name: "Ницца",
     link: "https://images.unsplash.com/photo-1503696967350-ad1874122058?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+    alt: "вытянутый песчаный пляж в городе у лазурного моря",
   },
   {
     name: "Сен-Тропе",
     link: "https://images.unsplash.com/photo-1608035057921-3c1e28e812f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    alt: "узкая улочка из брусчатки проходит между старых домов и ведет к морю",
   },
   {
     name: "Ле-Трепор",
     link: "https://images.unsplash.com/photo-1552426106-468a47fdd2cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1772&q=80",
+    alt: "высокий утес у моря",
   },
   {
     name: "Красное море",
     link: "https://images.unsplash.com/photo-1582623838120-455da222cdc7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80",
+    alt: "подводный морской мир, разнообразные рыбы и кораллы",
   },
   {
     name: "Этрета",
     link: "https://images.unsplash.com/photo-1505753065532-68713e211a3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    alt: "песчаное побережье и утес с природной аркой у лазурного моря",
   },
   {
     name: "Море Кортеса",
     link: "https://images.unsplash.com/photo-1615695478392-af177ac550b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1774&q=80",
+    alt: "кактусы и лодка у пляжа",
   },
 ];
 
@@ -57,68 +65,77 @@ const initialCards = [
 
 function togglePopUp(popUp) {
   popUp.classList.toggle("pop-up_opened");
-  if (popUp === popUpProfile) {
-    nameInput.value = currentName.textContent;
-    bioInput.value = currentBio.textContent;
-  }
 }
 
-function profileFormSubmitHandler(evt) {
+function togglePopUpProfile() {
+  nameInput.value = currentName.textContent;
+  bioInput.value = currentBio.textContent;
+  togglePopUp(popUpProfile);
+}
+
+function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   currentName.textContent = nameInput.value;
   currentBio.textContent = bioInput.value;
   togglePopUp(popUpProfile);
 }
 
-function createGalleryItem(name, link) {
-  const galleryItem = imageTemplate
-    .querySelector(".gallery__item")
-    .cloneNode(true);
-  const image = galleryItem.querySelector(".gallery__image");
-  const itemName = galleryItem.querySelector(".gallery__item-name")
-  const likeButton = galleryItem.querySelector(".gallery__like-button");
+function createCard(name, link, alt) {
+  const card = imageTemplate.querySelector(".gallery__item").cloneNode(true);
+  const image = card.querySelector(".gallery__image");
+  const itemName = card.querySelector(".gallery__item-name");
+  const likeButton = card.querySelector(".gallery__like-button");
 
   itemName.textContent = name;
   image.src = link;
+  image.alt = alt;
 
-  image.addEventListener("click", () => openImage(image));
-  
+  image.addEventListener("click", () => openImage(name, link, alt));
+
   likeButton.addEventListener("click", () =>
     likeButton.classList.toggle("gallery__like-button_active")
   );
-  galleryItem
+  card
     .querySelector(".gallery__item-remove-button")
     .addEventListener("click", () => galleryItem.remove());
+  return card;
+}
 
+function createGalleryItem(name, link, alt) {
+  const galleryItem = createCard(name, link, alt);
   gallery.prepend(galleryItem);
 }
 
-function imageFormSubmitHandler(evt) {
+function handleImageFormSubmit(evt) {
   evt.preventDefault();
   createGalleryItem(imageNameInput.value, imageLinkInput.value);
   formElementImage.reset();
   togglePopUp(popUpImage);
 }
 
-function openImage(image) {
-  popUpImageView.querySelector(".image-view__image").src = image.src;
-  popUpImageView.querySelector(".image-view__name").textContent =
-    image.parentElement.querySelector(".gallery__item-name").textContent;
+function openImage(name, link, alt) {
+  image.src = link;
+  image.alt = alt;
+  imageName.textContent = name;
   togglePopUp(popUpImageView);
 }
 
-// **** FUNCTION CALLS ****//
+// **** FUNCTION CALLS **** //
 
-initialCards.forEach((item) => createGalleryItem(item["name"], item["link"]));
+initialCards.forEach((item) =>
+  createGalleryItem(item["name"], item["link"], item["alt"])
+);
 
-editProfileButton.addEventListener("click", () => togglePopUp(popUpProfile));
-formElementProfile.addEventListener("submit", profileFormSubmitHandler);
+editProfileButton.addEventListener("click", () =>
+  togglePopUpProfile(popUpProfile)
+);
+formElementProfile.addEventListener("submit", handleProfileFormSubmit);
 closeButtonProfileForm.addEventListener("click", () =>
   togglePopUp(popUpProfile)
 );
 
 addImageButton.addEventListener("click", () => togglePopUp(popUpImage));
-formElementImage.addEventListener("submit", imageFormSubmitHandler);
+formElementImage.addEventListener("submit", handleImageFormSubmit);
 closeButtonImageForm.addEventListener("click", () => togglePopUp(popUpImage));
 
 closeImageViewButton.addEventListener("click", () =>
